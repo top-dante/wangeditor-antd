25e4b629fb534617d777b815a345698146c3cc23
top-dante <763093576@qq.com>
2020-05-21 17:37:09 +0800

    added eslint代码检查 modified 只需要加载js文件即可

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars": 0,
+        "no-undef":0
+    }
+};
